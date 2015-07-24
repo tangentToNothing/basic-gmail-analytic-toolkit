dd6b7eebffb36e5c505af0eb7991d850ceddfa9b
tangentToNothing <nicholasmellen@gmail.com>
2015-07-24 16:58:13 -0400

    Version 0.1

--- gmail-analytic-toolkit/script.php
+++ gmail-analytic-toolkit/script.php
@@ -7,6 +7,13 @@ $client = new Google_Client();
 $client->setAuthConfigFile('client_secret.json');
 $client->addScope('https://www.googleapis.com/auth/gmail.readonly');
 */
+/*
+ * 
+ * trying to be a better commenter.
+ * 
+ * this script is called upon the login page's form submit.
+ * it checks for an active session, and send the user to oauth2callback if there is no current session
+ */
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
     
   $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/gmail-analytic-toolkit/home.php';
